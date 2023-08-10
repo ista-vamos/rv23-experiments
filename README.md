c12f4e093367835647893f106b530ba5cb7154f3
Marek Chalupa <mchqwerty@gmail.com>
2023-08-10 09:15:46 +0200

    Add a script to run experiments

--- README.md
+++ README.md
@@ -1,16 +1,40 @@
 # rv23-experiments
 Experiments from the paper "Monitoring Hyperproperties With Prefix Transducers" accepted at RV'23
 
-## Build
+## Building
 
-```
+### Requirements
+
+### Configuration & Build
+
+```shell
+# clone the repo
 git checkout https://github.com/ista-vamos/rv23-experiments
 cd rv23-experiments
 
-# initialize vamos-buffers
+# initialize and build vamos-buffers
 git submodule update --init -- vamos-buffers
+cd vamos-buffers
+cmake . -DCMAKE_BUILD_TYPE=Release
+make -j4
+cd -
 
 # configure and build the project
 cmake .
 make -j4
 ```
+
+## Running
+
+In the top level directory is the script `run.sh` that takes as an argument either
+`rand` to run experiments on random traces, or `1t` to run experiments on
+multiple instances of the same trace. So run the experiments as follows:
+
+```
+./run.sh rand
+./run.sh 1t
+```
+
+By default, the script repeats the experiments 10 times (can be modified by changing
+the `REP` variable in the script). If you run the script multiple times, it will
+prompt the user if the old results should be overwritten or not.
---
+++ run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+#################################################
+# How many times to repeat the experiments?
+REP=10
+#################################################
+
+if [ "$1" != "rand" -a "$1" != "1t" ]; then
+	echo "Usage: $0 [rand | 1t]"
+	echo "'rand' means run on random traces, '1t' are experiments with instances of a single trace"
+	exit 1
+fi
+
+TOPDIR=$(readlink -f $(dirname $0))
+OUT="$TOPDIR/results_$1.csv"
+
+echo "Output file: $OUT"
+
+cd $TOPDIR/mpt-monitors
+
+if [ -e $OUT ]; then
+	echo "The output file $OUT exists."
+	echo -e "\033[0;34mShould I delete it? (If no, I will append results)[y/n]\033[0m"
+	read Q
+	while [ "$Q" != "y" -a "$Q" != "n" ]; do
+		echo "Please type 'y' or 'n']"
+		read Q
+	done
+
+	if [ "$Q" == "y" ]; then
+		echo "Overwriting $OUT"
+		rm $OUT
+		touch $OUT
+	else
+		echo "Appending to $OUT"
+	fi
+fi
+
+echo "---------------------------------------------"
+echo "Starting experiments, doing $REP repetitions."
+echo "---------------------------------------------"
+for i in $(seq 1 $REP); do
+	python ./run.py "monitor_$1" | tee --append $OUT
+done
+echo "---------------------------------------------"
+echo "Done."
+echo "---------------------------------------------"
